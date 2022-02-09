cee41de8b331b7b0020c666309bddb8689273ad2
L1oid <71085120+L1oid@users.noreply.github.com>
2022-02-09 19:16:20 +0800

    Add files via upload

--- Lab1/Ex6.py
+++ Lab1/Ex6.py
@@ -1,6 +1,20 @@
-a = input()
-list(a)
-if ((int(a[0]) + int(a[2])) / 2 == int(a[1])):
-    print('Красивое число')
+num = int(input())
+c = int(num % 10)
+b = int((num % 100) / 10)
+a = int(num / 100)
+if a > c:
+    temp = a
+    a = c
+    c = temp
+if a > b:
+    temp = a
+    a = b
+    b = temp
+if c < b:
+    temp = c
+    c = b
+    b = temp
+if (a + c) / 2 == b:
+    print("Красивое!")
 else:
-    print('Обычное число')
+    print("Обычное!")
