68efbbc73013c401c8e5e2907438e60501fa1245
L1oid <71085120+L1oid@users.noreply.github.com>
2022-03-02 18:00:29 +0700

    Update Ex1.py

--- Lab4/Ex1.py
+++ Lab4/Ex1.py
@@ -1,1 +1,8 @@
-
+check = dict()
+for word in input().split():
+    if word in check:
+        check[word] = check.get(word) + 1
+        print(check.get(word))
+    else:
+        check[word] = 0
+        print(check.get(word))
