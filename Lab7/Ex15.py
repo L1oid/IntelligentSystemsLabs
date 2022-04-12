707e62dee8009450b6744a0754ed118753b90a14
L1oid <71085120+L1oid@users.noreply.github.com>
2022-04-12 18:05:30 +0700

    add ex

---
+++ Lab7/Ex15.py
@@ -0,0 +1,33 @@
+class Digits:
+    def __init__(self, a):
+        self.a = [int(x) for x in a.split()]
+
+    def make_negative(self):
+        for i in range(len(self.a)):
+            self.a[i] = -abs(self.a[i])
+
+    def square(self):
+        for i in range(len(self.a)):
+            self.a[i] = (self.a[i]) ** 2
+
+    def strange_command(self):
+        for i in range(len(self.a)):
+            if self.a[i] % 5 == 0:
+                self.a[i] += 1
+
+    def stringify(self):
+        for i in range(len(self.a)):
+            self.a[i] = str(self.a[i])
+
+
+d = Digits(input())
+for i in range(int(input())):
+    s = input()
+    if s == 'make_negative':
+        d.make_negative()
+    elif s == 'square':
+        d.square()
+    else:
+        d.strange_command()
+d.stringify()
+print(*d.a)
